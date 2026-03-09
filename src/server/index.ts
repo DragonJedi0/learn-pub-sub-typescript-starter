d867c565c9e3d8f761f02c2d5fe2f027fbde1a59
Samuel Ybarra <sybarra2076@gmail.com>
2026-03-09 12:48:39 -0500

    Ch 3 Lesson 5

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,24 @@
+import amqp from "amqplib";
+import { publishJSON } from "../internal/pubsub/pubsub.js";
+import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+
 async function main() {
   console.log("Starting Peril server...");
+  const rabbitUrl = "amqp://guest:guest@localhost:5672/";
+
+  const conn = await amqp.connect(rabbitUrl);
+  console.log("Connection successful.");
+
+  const confirm = await conn.createConfirmChannel();
+
+  await publishJSON(confirm, ExchangePerilDirect, PauseKey, { isPaused: true });
+
+  process.on('exit', () => {
+    console.log("\nExit signal detected.\nShutting down server...");
+    conn.close();
+    console.log("Sever safely shut down.");
+  });
+
 }
 
 main().catch((err) => {
